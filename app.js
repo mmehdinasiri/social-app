881754b2ecab7c78fc1f5de7e20cbb8be04e1faa
mmehdinasiri <m.mehdi.nasiri@gmail.com>
2019-07-29 18:44:09 +0430

    add env and server

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// db
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => {console.log('DB Connected')});
+
+mongoose.connection.on('error', err => {
+  console.log(`DB connection error : ${err.message}`)
+})
 
 // brign in routes
 const postRoutes = require('./routes/post')
@@ -13,7 +24,7 @@ app.use(morgan("dev"));
 app.use('/', postRoutes)
 
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`A node js api is listening on port : ${port}`)
 });
