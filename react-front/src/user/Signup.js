50f8406e6f079f333e6703a6763ed379456a9375
mmehdinasiri <m.mehdi.nasiri@gmail.com>
2019-08-28 01:22:23 +0430

    finish signup

--- react-front/src/user/Signup.js
+++ react-front/src/user/Signup.js
@@ -7,10 +7,15 @@ class Signup extends Component {
       name: '',
       email: '',
       password: '',
-      error: ''
+      error: '',
+      open: false
     }
   }
   handleChange = (name) => (event) => {
+    this.setState({
+      error: '',
+      open: false
+    })
     this.setState( {[name] : event.target.value });
   }
   clickSubmit = (event) => {
@@ -21,27 +26,40 @@ class Signup extends Component {
       email,
       password
     }
-    fetch("http://localhost:8080/signup", {
-      method: "POST",
-      headers: {
-        'Accept': "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(user)
-    })
-    .then(response => {
-      return response.json();
-    })
-    .catch(err =>{
-       console.log(err)
-     });
-  }
-  render() {
-    const {name, email, password} = this.state;
-    return (
-      <div className="container">
-        <h2 className="mt-5 mb-5">Signup</h2>
+    this.signup(user).then(data => {
+      if(data.error){
+        this.setState({error: data.error})
+      }else{
 
+        this.setState({
+          error: '',
+          name: '',
+          email: '',
+          password: '',
+          open: true
+        })
+      }
+    })
+    
+  }
+  
+  signup = (user) => {
+    return fetch("http://localhost:8080/signup", {
+              method: "POST",
+              headers: {
+                'Accept': "application/json",
+                "Content-Type": "application/json"
+              },
+              body: JSON.stringify(user)
+            })
+            .then(response => {
+              return response.json();
+            })
+            .catch(err =>{
+              console.log(err)
+            });
+  }
+  signupForm = (name, email, password) => (
         <form>
           <div className="form-group">
             <label className="text-muted">Name</label>
@@ -60,6 +78,19 @@ class Signup extends Component {
 
           <button className="btn btn-raised btn-primary" onClick={this.clickSubmit}>Submit</button>
         </form>
+  ) 
+  render() {
+    const {name, email, password, error, open} = this.state;
+    return (
+      <div className="container">
+        <h2 className="mt-5 mb-5">Signup</h2>
+        <div className="alert alert-danger" style={{display : error ? '' : 'none'}}>
+          {error}
+        </div>
+        <div className="alert alert-info" style={{display : open ? '' : 'none'}}>
+          welcome to social app please login 
+        </div>
+          {this.signupForm(name, email, password)} 
       </div>
     )
   }
